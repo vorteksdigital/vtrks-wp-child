44d2f17b2eaa3e899dd2ba0837d7d485a8aad987
joshuacalebdavids <joshuadavids.jcd@gmail.com>
2025-07-11 14:06:21 +0200

    Added SVG Support

--- wp-content/themes/vorteks-digital-child-theme/helpers/utilities/utils.php
+++ wp-content/themes/vorteks-digital-child-theme/helpers/utilities/utils.php
@@ -82,3 +82,10 @@ function vrtks_enqueue_assets() {
     );
 }
 add_action('wp_enqueue_scripts', 'vrtks_enqueue_assets');
+
+// Add SVG Support
+function vrtks_allow_svg_uploads($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'vrtks_allow_svg_uploads');
